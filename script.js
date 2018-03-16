9d41d1f2bdaf573079e48b9909cee29893fc2c92
Syknapse <syk.houdeib@gmail.com>
2018-03-17 00:48:36 +0100

    Add dynamic elements
    Implement logic for all major features.
    Dynamically generate list of categories, their subcategories, view section, and clickable links.

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let postalCodeInput = 28010;
 let selectedMarket;
 let selectedMarketColor;
 let selectedMarketIcon;
+const header = document.getElementById('nav-header');
+const marketIcon = document.querySelector('#nav-header img');
+const marketName = document.querySelector('#nav-header h1');
+const marketPostalCode = document.querySelector('#nav-header p');
+const productCategories = document.getElementById('product-categories');
 
 fetch(tokenAPI)
 .then(response => {
@@ -15,25 +20,73 @@ fetch(tokenAPI)
             // console.log(token);
         })
 })
+.then(
+    fetch(marketAPI)
+    .then(response => {
+            response.json().then(data => {
+                selectedMarket = data.markets.find(thisMarket);
+                marketIcon.setAttribute('src', selectedMarket.icon);
+                marketName.innerText = `${selectedMarket.name}`;
+                marketPostalCode.innerText = `Comprando en ${postalCodeInput}`;
+                header.style.backgroundColor = `rgb(${selectedMarket.color})`;
+            })
+    })
+)
+.then(
+    fetch(productsAPI)
+    .then(response => {
+            response.json().then(data => {
+                data.categories.forEach(category => {
+                    let categorySection = document.createElement('li');
+                    let categoryIcon = document.createElement('img');
+                    let categoryName = document.createElement('strong');
+                    let subcategories = document.createElement('ul');
+                    // let subcategorySection = document.createElement('li');
+                    // let subcategoryName = document.createElement('a');
+                    let subcategoryDisplay = '';
 
-fetch(marketAPI)
-.then(response => {
-        response.json().then(data => {
-            selectedMarket = data.markets.find(thisMarket);
-            console.log(selectedMarket.name);
-            console.log(selectedMarket.color);
-            console.log(selectedMarket.icon);
-        })
-})
+                    productCategories.appendChild(categorySection);
+                    categorySection.appendChild(categoryIcon);
+                    categorySection.appendChild(categoryName);
+                    categorySection.appendChild(subcategories);
+                    categoryIcon.setAttribute('src', category.icon);
+                    categoryName.innerText = category.name;
 
-fetch(productsAPI)
-.then(response => {
-        response.json().then(data => {
-            console.log(data);
-            console.log(data.categories[0]);
-            console.log(data.categories["0"].categories["0"].icon);
-        })
-})
+                    // console.log(category.categories);
+
+                    category.categories.forEach(subcategory => {
+                        let subcategoryUrl = `/tienda/${marketInput}/${category.shortcut}/${subcategory.shortcut}`;
+
+                        subcategoryDisplay +=
+                        `<li>
+                            <a href="${subcategoryUrl}">${subcategory.name}</a>
+                        </li>`;
+
+                        /* subcategories.appendChild(subcategorySection);
+                        subcategorySection.appendChild(subcategoryName);
+                        subcategoryName.setAttribute('href', subcategoryUrl);
+                        subcategoryName.innerText = subcategory.name; */
+
+                        // console.log(subcategory);
+                    })
+                    let seeSection = document.createElement('li');
+                    let seeSectionLink = document.createElement('a');
+                    let categoryUrl = `/tienda/${marketInput}/${category.shortcut}`;
+
+                    subcategories.innerHTML = subcategoryDisplay;
+                    subcategories.insertBefore(seeSection, subcategories.firstChild);
+                    seeSection.appendChild(seeSectionLink);
+                    seeSectionLink.setAttribute('href', categoryUrl);
+                    seeSectionLink.innerText = 'Ver toda la sección';
+                })
+                console.log(data);
+                // console.log(data.categories[0]);
+                // console.log(data.categories["0"].categories["0"].icon);
+            })
+    })
+)
+
+
 
 function thisMarket(market) {
     return market.name === `${marketInput}`;
